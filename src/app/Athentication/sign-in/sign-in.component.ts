ac8f36a860d04fa7c1da5d763764c715df27acee
simlanders <simeonlanders2022.cs.grad@gmail.com>
2025-07-29 02:27:32 -0400

    Unused imports removed

--- src/app/Athentication/sign-in/sign-in.component.ts
+++ src/app/Athentication/sign-in/sign-in.component.ts
@@ -1,10 +1,8 @@
 import { Component} from '@angular/core';
-import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'sign-in',
   templateUrl: './sign-in.component.html',
-  imports: [NgOptimizedImage],
 })
 export class SignIn {
  
