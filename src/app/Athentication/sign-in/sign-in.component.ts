cb5351d8e7c35fd385a17e1ca86145d8cca30fe6
simlanders <simeonlanders2022.cs.grad@gmail.com>
2025-07-30 00:47:43 -0400

    Sign-in background imaged changed Styles '.button-row' and 'my-sign-in-button' added to global styles.css sign-in.component.ts edited to control sign-in visibility

--- src/app/Athentication/sign-in/sign-in.component.ts
+++ src/app/Athentication/sign-in/sign-in.component.ts
@@ -1,12 +1,14 @@
 import { Component} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import { CommonModule, NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'sign-in',
   templateUrl: './sign-in.component.html',
-  imports:[MatButtonModule]
+  imports:[MatButtonModule,NgIf,CommonModule]
 })
 export class SignIn {
+  isVisible: boolean = true;
  
 }
