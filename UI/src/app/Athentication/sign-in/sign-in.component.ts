2db057d225a71c0880ac1edcc4891a1376108584
simlanders <simeonlanders2022.cs.grad@gmail.com>
2025-07-30 20:35:07 -0400

    Switched all angular project files to new UI folder

---
+++ UI/src/app/Athentication/sign-in/sign-in.component.ts
@@ -0,0 +1,34 @@
+import { Component, computed, OnInit, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule} from '@angular/common';
+
+
+@Component({
+  selector: 'sign-in',
+  templateUrl: './sign-in.component.html',
+  imports: [MatButtonModule,CommonModule]
+})
+
+export class SignIn implements OnInit {
+  isSignInAndSignUpButtonsVisible = signal(true);
+  isSignInFormVisible = computed(() => {
+  if (this.isSignInAndSignUpButtonsVisible()) {
+    return false;
+  } else {
+    return true;
+  }
+});
+
+  ngOnInit() {
+    console.log('ngOnInit() => is Sign In And Sign Up Buttons Visible: ' + this.isSignInAndSignUpButtonsVisible());
+    console.log('ngOnInit() => is Sign In Form Visible: ' + this.isSignInFormVisible());
+  }
+
+  signInButtonClicked() {
+    this.isSignInAndSignUpButtonsVisible.update(value => false);
+    console.log('signInButtonClicked() => is Sign In And Sign Up Buttons Visible: ' + this.isSignInAndSignUpButtonsVisible());
+    console.log('signInButtonClicked() => is Sign In Form Visible: ' + this.isSignInFormVisible());
+    
+  }
+  
+}
---
+++ UI/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SignIn } from './Athentication/sign-in/sign-in.component';
+
+export const routes: Routes = [
+
+     {
+    path: '',
+    component: SignIn,
+  }
+
+  ];
+
+
